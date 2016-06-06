9332cc3ac2c3bf2a39ff0a174c23edb359ec5a57
Diego Campos Aquino <caquinodiego@crece.uss.edu.pe>
2016-06-06 14:08:06 -0500

    regx

--- ahorcado.py
+++ ahorcado.py
@@ -1,2 +1,12 @@
 import re
 import sys
+## REGEX
+##-------------------------
+#CADENA (..*?)
+#CADENA ([^asd]) sin a,s,d
+#INICIO DE CADENA /\b()/
+#FIN DE LA CADENA /()\b/
+##-------------------------
+
+#obteniendo argumento : python ahorcado.py <arg1>
+cadena = sys.argv[1]
